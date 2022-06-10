9f3b2cdd1705f14ac87b5c0144aa9d314c1aaefc
MatiasOrozcoPrendes <matias.orozco.prendes@gmail.com>
2022-06-09 21:33:23 -0300

    Agrego el componente Header. Empiezo con el Proyecto. Agrego la carpeta imagenes con todo lo necesario.

---
+++ portfolio/src/Componentes/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header' >
+        <div>
+            <a className='Titulo' href='#SobreMi'><h1>Matias Orozco</h1></a>
+        </div>
+        <div>
+    
+        </div>
+        <div className='UnaLinea'>
+          
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
